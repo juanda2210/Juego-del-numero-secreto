13d8cbfc0e4bbd60073d2087c599fb075e9e2333
juanda2210 <juandarias2204@gmail.com>
2024-07-18 21:24:32 -0500

    Versiòn final juego del numero secreto

---
+++ app.js
@@ -0,0 +1,69 @@
+let numeroSecreto;
+let i;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    if (numeroDeUsuario === numeroSecreto) {
+        if (i == 1) {
+            asignarTextoElemento('p', 'Acertaste el número en ' + i + ' vez');
+        } else {
+            asignarTextoElemento('p', 'Acertaste el número en ' + i + ' veces');
+        }
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p', 'El número secreto es menor');
+        } else {
+            asignarTextoElemento('p', 'El número secreto es mayor');
+        }
+        i++;
+        limpiarCaja();
+    }
+
+    return;
+}
+
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = '';
+}
+
+function generarNumeroSecreto() {
+    let numeroGenerado = Math.floor(Math.random() * numeroMaximo) + 1;
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+    if (listaNumerosSorteados.length == numeroMaximo) {
+        asignarTextoElemento('p', 'Ya se sortearon todos los números posibles');
+    } else {
+        if (listaNumerosSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreto();
+        } else {
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
+
+}
+
+function condicionesIniciales() {
+    asignarTextoElemento('h1', 'Juego del número secreto');
+    asignarTextoElemento('p', 'Indica un número del 1 al ' + numeroMaximo);
+    numeroSecreto = generarNumeroSecreto();
+    i = 1;
+}
+
+function reiniciarJuego() {
+    limpiarCaja();
+    condicionesIniciales();
+    document.getElementById('reiniciar').setAttribute('disabled', 'true');
+}
+
+condicionesIniciales();
